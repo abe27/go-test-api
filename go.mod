f2338f203c68ee598002204a833e2b484d404301
Taweechai Yuenyang <krumii.it@gmailcom>
2022-07-06 12:59:37 +0700

    update route

--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.18
 require github.com/gofiber/fiber/v2 v2.34.1
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.12.1 // indirect
@@ -23,6 +28,7 @@ require (
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gorm.io/driver/mysql v1.3.4
 	gorm.io/driver/postgres v1.3.8 // indirect
 	gorm.io/gorm v1.23.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"strings"
 
 	handlers "github.com/abe27/api/v2/controllers"
 	"github.com/abe27/api/v2/database"
 	"github.com/abe27/api/v2/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"gorm.io/driver/postgres"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 func initDatabase() {
 	var err error
-	dsn := "host=" + os.Getenv("DBHOST") + " user=" + os.Getenv("DBUSER") + " password=" + os.Getenv("DBPASSWD") + " dbname=" + os.Getenv("DBNAME") + " port=" + os.Getenv("DBPORT") + ""
-	database.DBConn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// dsn := "host=" + os.Getenv("DBHOST") + " user=" + os.Getenv("DBUSER") + " password=" + os.Getenv("DBPASSWD") + " dbname=" + os.Getenv("DBNAME") + " port=" + os.Getenv("DBPORT") + ""
+	dns := "root:@tcp(127.0.0.1:3306)/godb?charset=utf8mb4&parseTime=True&loc=Local"
+	database.DBConn, err = gorm.Open(mysql.Open(dns), &gorm.Config{
+		SkipDefaultTransaction: true,
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "tbt_", // table name prefix, table for `User` would be `t_users`
+			SingularTable: false,  // use singular table name, table for `User` would be `user` with this option enabled
+			NoLowerCase:   false,  // skip the snake_casing of names
+			NameReplacer:  strings.NewReplacer("CID", "Cid"),
+		},
+	})
 	if err != nil {
 		panic("Failed to connect to database")
 	}
@@ -25,9 +37,15 @@ func initDatabase() {
 	fmt.Println("Migrated DB")
 }
 
+func setUpRouter() {
+
+}
+
 func main() {
 	app := fiber.New()
 	initDatabase()
+	app.Use(logger.New())
+	app.Use(requestid.New())
 	// Or extend your config for customization
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
